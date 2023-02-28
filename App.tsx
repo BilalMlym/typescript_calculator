62f270b5574a5e5d84af57cbac05b84775eda3d6
Bilal Mülayim <bilalmulayim@Bilal-MacBook-Pro.local>
2023-02-28 16:23:54 +0300

    change design

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { SafeAreaView, StyleSheet, Switch, Text } from "react-native";
+import { Colors } from "./styles/colors";
+import { ThemeContext } from "./context/theme";
+import Calculator from "./screen/calculator";
+
+export default function App() {
+  const [theme, setTheme] = useState("light");
+  return (
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView
+        style={
+          theme === "light"
+            ? styles.container
+            : [styles.container, { backgroundColor: "black" }]
+        }
+      >
+        <Switch
+          value={theme === "dark"}
+          onValueChange={() => setTheme(theme === "light" ? "dark" : "light")}
+        />
+        <Calculator />
+      </SafeAreaView>
+    </ThemeContext.Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.light,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+});
